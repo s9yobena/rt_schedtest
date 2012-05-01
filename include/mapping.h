55a67b0876f3a36cee003260b235571e8ec56326
Youcef Amine Benabbas <amine.benabbas@googlemail.com>
2012-05-01 19:54:46 +0200

    Deleted mappping.h, mappping.c

--- include/mapping.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MAPPING_H_
-#define _MAPPING_H_
-
-int map_file(const char* filename, void **addr, size_t *size);
-
-int map_file_rw(const char* filename, void **addr, size_t *size);
-
-#endif
-
